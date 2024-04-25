a9cb60b0d28e17fe926eddb887a1641db46330dd
Adnir Andrade <adnir_andrade@hotmail.com>
2024-04-25 10:26:55 -0300

    Implemenet standard header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,23 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { Button } from "react-native";
 
 export default function _layout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        headerRight: () => (
+          <Button onPress={() => console.log("Clicked")} title="Update count" />
+        ),
+        title: "Sunshine Autos",
+      }}
+    ></Stack>
+  );
 }
