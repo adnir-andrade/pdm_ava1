9393da8f66ad04c9438280297cb9ec6941814175
Adnir Andrade <adnir_andrade@hotmail.com>
2024-04-26 16:31:12 -0300

    Implement carSelection styles

--- src/app/carSelection.tsx
+++ src/app/carSelection.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import data from "../services/data";
 import HeaderWithTitle from "../components/headers/HeaderWithMenu";
 import Background from "../components/ui/Background";
+import Card from "../components/containers/Card";
 
 const uniqueBrands = [...new Set(data.map((car) => car.brand))];
 
@@ -17,20 +18,28 @@ export default function carSelection() {
     <View>
       <Background>
         <HeaderWithTitle title="Sunshine Autos" />
-        <SectionList
-          sections={carsByBrand}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.title}>ID: {item.id}</Text>
-              <Text style={styles.title}>Model: {item.model}</Text>
-              <Text style={styles.title}>Year: {item.year}</Text>
-            </View>
-          )}
-          renderSectionHeader={({ section: { title } }) => (
-            <Text style={styles.header}>{title}</Text>
-          )}
-        />
+        <View style={styles.sectionContainer}>
+          <Card>
+            <SectionList
+              sections={carsByBrand}
+              keyExtractor={(item) => item.id.toString()}
+              renderSectionHeader={({ section: { title } }) => (
+                <View style={styles.headerContainer}>
+                  <Card>
+                    <Text style={styles.header}>{title}</Text>
+                  </Card>
+                </View>
+              )}
+              renderItem={({ item }) => (
+                <View style={styles.item}>
+                  <Text style={styles.title}>ID: {item.id}</Text>
+                  <Text style={styles.title}>Model: {item.model}</Text>
+                  <Text style={styles.title}>Year: {item.year}</Text>
+                </View>
+              )}
+            />
+          </Card>
+        </View>
       </Background>
     </View>
   );
@@ -44,7 +53,25 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
   },
   title: {
-    fontSize: 12,
+    fontSize: 15,
+    color: "#fff",
+    textShadowColor: "cyan",
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 10,
   },
-  header: { fontSize: 32 },
+  header: {
+    fontSize: 32,
+    textAlign: "center",
+    color: "#fff",
+    textShadowColor: "purple",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 15,
+  },
+  headerContainer: {
+    paddingHorizontal: 10,
+    marginTop: 15,
+  },
+  sectionContainer: {
+    margin: 50,
+  },
 });
